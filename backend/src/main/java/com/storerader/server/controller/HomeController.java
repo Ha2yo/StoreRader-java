06c6b0fd6a1bdfd5b13c1fae66e7757c7fb0e4d7
Ha2yo <ha2yo1114@gmail.com>
2026-01-05 19:51:36 +0900

    fix: move gradle to backend directory
    - "Unable to access jarfile" 문제 해결

---
+++ backend/src/main/java/com/storerader/server/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.storerader.server.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+        public String index(){
+            return "index";
+    }
+}
