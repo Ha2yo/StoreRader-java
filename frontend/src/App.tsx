cac02cd7b4f96a90e6824f79bd61684c69470cd7
Ha2yo <ha2yo1114@gmail.com>
2026-01-08 00:38:25 +0900

    feat: Modify App.tsx
    - 기본 틀 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    localStorage.removeItem("lastSearchTerm");
+    localStorage.removeItem("selectedGoodName");
+
+    useEffect(() => {
+        const tryNavigate = async () => {
+            // 오프라인 상태에서는 초기 이동 중단
+
+            // 서버 상태가 정상일 때만 홈 화면으로 이동
+            if (status === "ok") {
+                localStorage.removeItem("lastSearchTerm");
+                localStorage.removeItem("selectedGoodName");
+                navigate("/home");
+            }
+        };
+
+        tryNavigate();
+    });
+
+    return (
+        <div className='container'>
+        </div>
+    );
 }
 
-export default App
+export default App;
