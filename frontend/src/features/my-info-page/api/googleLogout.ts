1651950b0c81f966910a6d4d7763dd5276ca2498
Ha2yo <ha2yo1114@gmail.com>
2026-01-08 23:59:07 +0900

    fix: delete unused module
    - 안 쓰이는 부분 삭제

--- frontend/src/features/my-info-page/api/googleLogout.ts
+++ frontend/src/features/my-info-page/api/googleLogout.ts
@@ -1,6 +1,2 @@
-import { signOut } from "@choochmeque/tauri-plugin-google-auth-api";
-
 export async function requestGoogleLogout() {
-  const accessToken = localStorage.getItem("accessToken") || undefined;
-  return signOut({ accessToken });
 }
