ab251d7360525860c55352830beadc98ff6d6756
Ha2yo <ha2yo1114@gmail.com>
2026-01-14 18:30:41 +0900

    feat: Add axios
    - 백엔드, 프론트 간 간단한 통신 테스트

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Home() {
+  const API_SERVER_URL = import.meta.env.VITE_API_SERVER_URL
+  const [hello, setHello] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_SERVER_URL)
+      .then(response => setHello(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
   return (
     <div className='container'>
       <h1>Home</h1>
+      <p> 백엔드 통신 테스트: {hello.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} </p>
     </div>
   );
 }
