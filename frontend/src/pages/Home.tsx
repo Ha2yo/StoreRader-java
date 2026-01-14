ad8c1509c33f770b774274c2e7bccdffca7a2912
Ha2yo <ha2yo1114@gmail.com>
2026-01-14 20:41:42 +0900

    fix: Edit home.tsx
    - 제대로 작동하게끔 수정

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,24 +2,19 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function Home() {
-  const API_SERVER_URL = import.meta.env.VITE_API_SERVER_URL
-  const [hello, setHello] = useState<string[]>([]);
+  const API_SERVER_URL = import.meta.env.VITE_API_SERVER_URL;
+  const [hello, setHello] = useState<string>("");
 
   useEffect(() => {
-    axios.get<string[]>(API_SERVER_URL)
-      .then(response => setHello(response.data))
-      .catch(error => console.log(error))
+    axios.get<string>(API_SERVER_URL)
+      .then(res => setHello(res.data))
+      .catch(console.error);
   }, []);
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Home</h1>
-      <p>백엔드 통신 테스트:</p>
-      <ul>
-        {hello.map((text, index) => (
-          <li key={index}>{text}</li>
-        ))}
-      </ul>
+      <p>백엔드 통신 테스트: {hello}</p>
     </div>
   );
 }
