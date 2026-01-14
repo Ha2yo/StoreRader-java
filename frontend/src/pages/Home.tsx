8b44c7009f185534f833be3a9585de70516a271e
Ha2yo <ha2yo1114@gmail.com>
2026-01-14 18:37:38 +0900

    fix: Specify type
    - 타입을 명시하여 오류 해결

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 
 function Home() {
   const API_SERVER_URL = import.meta.env.VITE_API_SERVER_URL
-  const [hello, setHello] = useState([]);
+  const [hello, setHello] = useState<string[]>([]);
 
   useEffect(() => {
-    axios.get(API_SERVER_URL)
+    axios.get<string[]>(API_SERVER_URL)
       .then(response => setHello(response.data))
       .catch(error => console.log(error))
   }, []);
@@ -14,7 +14,12 @@ function Home() {
   return (
     <div className='container'>
       <h1>Home</h1>
-      <p> 백엔드 통신 테스트: {hello.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} </p>
+      <p>백엔드 통신 테스트:</p>
+      <ul>
+        {hello.map((text, index) => (
+          <li key={index}>{text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
