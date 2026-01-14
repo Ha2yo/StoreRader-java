8dfa946759c1f9210ed57b528dbceb8b29621fd4
Ha2yo <ha2yo1114@gmail.com>
2026-01-14 23:08:10 +0900

    fix: prevent HTTPS to HTTP redirect
    - HTTPS 요청이 HTTP로 리다이렉트되던 문제 해결

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,10 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function Home() {
-  const API_SERVER_URL = import.meta.env.VITE_API_SERVER_URL;
   const [hello, setHello] = useState<string>("");
-
-  console.log("backend 주소: ",API_SERVER_URL);
 
   useEffect(() => {
     axios.get<string>("/api")
