caa5d14e6d5bdbe45769e3d232ea9f41a9403a9c
Ha2yo <ha2yo1114@gmail.com>
2026-01-30 01:11:38 +0900

    fix: component, configuration 어노테이션 추가

--- backend/src/main/java/com/storerader/server/admin/interceptor/AdminAuthInterceptor.java
+++ backend/src/main/java/com/storerader/server/admin/interceptor/AdminAuthInterceptor.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Component
 @RequiredArgsConstructor
 public class AdminAuthInterceptor implements HandlerInterceptor {
     private final AdminService adminService;
--- backend/src/main/java/com/storerader/server/admin/interceptor/WebConfig.java
+++ backend/src/main/java/com/storerader/server/admin/interceptor/WebConfig.java
@@ -1,9 +1,11 @@
 package com.storerader.server.admin.interceptor;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final AdminAuthInterceptor adminAuthInterceptor;
