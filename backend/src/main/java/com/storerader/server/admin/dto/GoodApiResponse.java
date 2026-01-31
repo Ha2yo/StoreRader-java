d3c488985a0d207100bbc310b3645b449d5d9da4
Ha2yo <ha2yo1114@gmail.com>
2026-02-01 06:43:20 +0900

    fix: 모든 goodsDTO에 예외처리

--- backend/src/main/java/com/storerader/server/admin/dto/GoodApiResponse.java
+++ backend/src/main/java/com/storerader/server/admin/dto/GoodApiResponse.java
@@ -1,8 +1,10 @@
 package com.storerader.server.admin.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JacksonXmlRootElement(localName = "response")
 public record GoodApiResponse(
         @JacksonXmlProperty(localName = "result")
