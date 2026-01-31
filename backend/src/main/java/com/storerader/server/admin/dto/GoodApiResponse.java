ef5e9439ce55c6e19dda0fbfa36ee0ee5d8b95e6
Ha2yo <ha2yo1114@gmail.com>
2026-02-01 06:24:57 +0900

    fix: goodsAPiResponse DTO 수정

--- backend/src/main/java/com/storerader/server/admin/dto/GoodApiResponse.java
+++ backend/src/main/java/com/storerader/server/admin/dto/GoodApiResponse.java
@@ -1,7 +1,9 @@
 package com.storerader.server.admin.dto;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
+@JacksonXmlRootElement(localName = "response")
 public record GoodApiResponse(
         @JacksonXmlProperty(localName = "result")
         GoodApiResult Apiresult
