85572788fbd878cb2be9878cd93cb76627055253
Ha2yo <ha2yo1114@gmail.com>
2026-02-05 20:29:10 +0900

    fix: PriceRepository에서 GoodEntity를 PriceEntity로 변경

--- backend/src/main/java/com/storerader/server/common/repository/PriceRepository.java
+++ backend/src/main/java/com/storerader/server/common/repository/PriceRepository.java
@@ -1,8 +1,8 @@
 package com.storerader.server.common.repository;
 
-import com.storerader.server.common.entity.GoodEntity;
+import com.storerader.server.common.entity.PriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PriceRepository extends JpaRepository<GoodEntity, Long> {
+public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
     boolean existsByInspectDay(String inspectDay);
 }
