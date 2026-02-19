d81264953ea630c2081c88653567d3f8483bcffe
Ha2yo <ha2yo1114@gmail.com>
2026-02-19 15:50:05 +0900

    fix: 마이그레이션 파일 오타 수정

--- backend/src/main/resources/db/migration/V14__CREATE_USER_SELECTION_LOG.sql
+++ backend/src/main/resources/db/migration/V14__CREATE_USER_SELECTION_LOG.sql
@@ -1,13 +1,26 @@
 CREATE TABLE user_selection_log (
+    -- 식별 번호
     id SERIAL PRIMARY KEY,
+
+    -- users 테이블의 식별번호
     user_id INTEGER NOT NULL,
+
+    -- 매장 id
     store_id INTEGER NOT NULL,
+
+    -- 상품 id
     good_id INTEGER NOT NULL,
+
+    -- 상품의 가격
     price INTEGER NOT NULL,
+
+    -- 유저의 선호 타입
     preference_type VARCHAR(10) NOT NULL,
+
+    -- 레코드 생성일시
     created_at TIMESTAMPTZ DEFAULT NOW(),
 
-    CONSTRAINT user_selection_)log_user_id_fkey
+    CONSTRAINT user_selection_log_user_id_fkey
         FOREIGN KEY (user_id)
         REFERENCES users(id)
         ON DELETE CASCADE
