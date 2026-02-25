060a208a57ca8ebed76e5e1a63d7520e20e8b72f
Ha2yo <ha2yo1114@gmail.com>
2026-02-25 17:47:43 +0900

    fix: 백엔드 UserSelectionLogreqDTO 잘못된 타입 수정

--- backend/src/main/java/com/storerader/server/domain/userSelectionLog/dto/UserSelectionLogReqDTO.java
+++ backend/src/main/java/com/storerader/server/domain/userSelectionLog/dto/UserSelectionLogReqDTO.java
@@ -2,7 +2,7 @@ package com.storerader.server.domain.userSelectionLog.dto;
 
 public record UserSelectionLogReqDTO  (
         Long storeId,
-        String goodId,
+        Number goodId,
         Integer price,
         String preferenceType
 ) {}
