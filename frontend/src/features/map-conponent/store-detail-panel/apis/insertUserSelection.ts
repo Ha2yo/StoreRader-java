5c2d39b56ca9b38fbcf07dbebfd85ea44a8a7e5a
Ha2yo <ha2yo1114@gmail.com>
2026-02-25 18:48:01 +0900

    fix: 프론트 insertUserSelection 파일 예외 처리 수정

--- frontend/src/features/map-conponent/store-detail-panel/apis/insertUserSelection.ts
+++ frontend/src/features/map-conponent/store-detail-panel/apis/insertUserSelection.ts
@@ -4,13 +4,12 @@ import type { SelectionPayload } from "../types/SelectionPayload";
 export async function insertUserSelection(payload: SelectionPayload) {
   try {
     await apiClient.post(
-      "/insert/user-selection", payload,
-      { withCredentials: true }
-    );
+      "/insert/user-selection", payload
+        );
     console.log("로그인 사용자 - 매장 선택 로그 기록");
 
   } catch (error: any) {
-    if (error.response.status === 500) {
+    if (error.response.status === 400) {
       console.log("비로그인 사용자 - 로그 저장 스킵");
 
     }
