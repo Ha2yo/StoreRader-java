eaff920db507752fff53b374a238f18f13a4fcc5
Ha2yo <ha2yo1114@gmail.com>
2026-03-01 02:08:04 +0900

    chore: 백엔드 test-controller 파일 깔끔하게 수정

--- backend/src/main/java/com/storerader/server/test/TestController.java
+++ backend/src/main/java/com/storerader/server/test/TestController.java
@@ -1,23 +1,22 @@
 package com.storerader.server.test;
 
-import com.storerader.server.common.entity.UserEntity;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Tag(name = "테스트 API", description = "Swagger 테스트용 API")
 @RestController
 @RequestMapping("/test")
 public class TestController {
-
     private final TestService testService;
 
-    @Operation(summary = "백엔드 응답 테스트", description = "간단한 문자열을 전송함으로써 서버의 상태를 점검합니다.")
-    @RequestMapping("/")
+    @Operation(
+            summary = "백엔드 응답 테스트",
+            description = "간단한 문자열을 전송함으로써 서버의 상태를 점검합니다."
+    )
+    @GetMapping("/")
     public String index() {
         return "index";
     }
