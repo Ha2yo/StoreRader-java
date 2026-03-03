13f8aea9f7fafecfe3838f896e266499a85b139c
Ha2yo <ha2yo1114@gmail.com>
2026-03-03 18:02:45 +0900

    chore: swagger 홈 화면 안내 메시지 변경

--- backend/src/main/java/com/storerader/server/config/SwaggerConfig.java
+++ backend/src/main/java/com/storerader/server/config/SwaggerConfig.java
@@ -17,8 +17,8 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
+                .title("StoreRader API")
+                .description("StoreRader 서비스의 REST API 문서입니다.")
                 .version("1.0.0");
     }
 }
