2f0b2d7926f95f6923eb71e79184e23981d73275
Ha2yo <ha2yo1114@gmail.com>
2026-03-04 00:56:56 +0900

    refactor: 백엔드 test-service 삭제 및 test-controller 개선

--- backend/src/main/java/com/storerader/server/test/controller/TestController.java
+++ backend/src/main/java/com/storerader/server/test/controller/TestController.java
@@ -12,8 +12,13 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
     private final TestService testService;
 
+    /**
+     * 서버가 정상적으로 동작하는지 확인하기 위한 테스트 엔드포인트
+     *
+     * @return 서버 상태 확인용 문자열
+     */
     @Operation(
-            summary = "백엔드 응답 테스트",
+            summary = "백엔드 응답 테스트용 API입니다.",
             description = "간단한 문자열을 전송함으로써 서버의 상태를 점검합니다."
     )
     @GetMapping("/")
