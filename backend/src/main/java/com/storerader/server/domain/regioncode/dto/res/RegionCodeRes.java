3e3767c87711566f2729927541eee062edabfaaf
Ha2yo <ha2yo1114@gmail.com>
2026-03-06 21:03:38 +0900

    docs: 일관성 위하여 줄바꿈 추가

--- backend/src/main/java/com/storerader/server/domain/regioncode/dto/res/RegionCodeRes.java
+++ backend/src/main/java/com/storerader/server/domain/regioncode/dto/res/RegionCodeRes.java
@@ -7,10 +7,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record RegionCodeRes(
         @Schema(description = "지역 코드", example = "020100000")
         String code,
+
         @Schema(description = "지역 이름", example = "서울특별시")
         String name,
+
         @Schema(description = "부모 코드", example = "020000000")
         String parentCode,
+        
         @Schema(description = "지역 레벨", example = "1")
         Integer level
 ) {
