3fa718610d44ea04b13bf96316a99d0b7c6c7cbd
Ha2yo <ha2yo1114@gmail.com>
2026-03-09 03:41:05 +0900

    fix: RestControllerAdvice 어노테이션 추가

--- backend/src/main/java/com/storerader/server/common/exception/CustomExceptionHandler.java
+++ backend/src/main/java/com/storerader/server/common/exception/CustomExceptionHandler.java
@@ -4,11 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
+@RestControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(value = CustomException.class)
